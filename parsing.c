aabd37509e1a7bc313d62c99ca56b8fe4a4a0c8e
Saad Sher Alam <saadsheralamfkah@gmail.com>
2024-07-08 16:47:22 +0500

    add built-in functions list head tail eval join

--- parsing.c
+++ parsing.c
@@ -54,6 +54,13 @@ lval* lval_eval(lval* v);
 lval* lval_pop(lval* v, int i); 
 lval* lval_take(lval* v, int i); 
 lval* builtin_op(lval* a, char* op); 
+lval* builtin_head(lval* a); 
+lval* builtin_tail(lval* a); 
+lval* builtin_list(lval* a);
+lval* builtin_eval(lval* a); 
+lval* builtin_join(lval* a); 
+lval* lval_join(lval* x, lval* y); 
+lval* builtin(lval* a, char* func); 
 
 
 // create pointer for lval num type 
@@ -246,7 +253,7 @@ lval* lval_eval_sexpr(lval* v) {
     return lval_err("S-expression Does not start with symbol!");
   }
 
-  lval* result = builtin_op(v, f->sym);
+  lval* result = builtin(v, f->sym);
   lval_del(f);
   return result;
 }
@@ -318,7 +325,85 @@ lval* builtin_op(lval* a, char* op) {
   lval_del(a); return x;
 }
 
+// define macro for error checking in builtin functions
+#define LASSERT(args, cond, err) \
+  if (!(cond)) { lval_del(args); return lval_err(err); }
 
+lval* builtin_head(lval* a){
+  // a should contain a valid q expr
+  // q expr should not be empty 
+  // head should only receive 1 argument  
+
+  LASSERT(a, a->count == 1, "Function 'head' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'head' passed incorrect type!");
+  LASSERT(a, a->cell[0]->count != 0, "Function 'head' passed {}!");
+
+  lval* v = lval_take(a, 0);
+  while (v->count > 1) { lval_del(lval_pop(v, 1)); }
+  return v;
+}
+
+lval* builtin_tail(lval* a){
+  LASSERT(a, a->count == 1, "Function 'tail' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'tail' passed incorrect type!");
+  LASSERT(a, a->cell[0]->count != 0, "Function 'tail' passed {}!");
+
+  lval* v = lval_take(a, 0);
+  lval_del(lval_pop(v, 0));
+  return v;
+}
+
+lval* builtin_list(lval* a) {
+  a->type = LVAL_QEXPR;
+  return a;
+}
+
+lval* builtin_eval(lval* a) {
+  LASSERT(a, a->count == 1, "Function 'eval' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR, "Function 'eval' passed incorrect type!");
+
+  // change qexpr type to sexpr and evaluate
+  lval* x = lval_take(a, 0);
+  x->type = LVAL_SEXPR;
+  return lval_eval(x);
+}
+
+lval* builtin_join(lval* a) {
+
+  for (int i = 0; i < a->count; i++) {
+    LASSERT(a, a->cell[i]->type == LVAL_QEXPR, "Function 'join' passed incorrect type.");
+  }
+
+  lval* x = lval_pop(a, 0);
+
+  while (a->count) {
+    x = lval_join(x, lval_pop(a, 0));
+  }
+
+  lval_del(a);
+  return x;
+}
+
+lval* lval_join(lval* x, lval* y) {
+
+  while (y->count) {
+    x = lval_add(x, lval_pop(y, 0));
+  }
+
+  lval_del(y);
+  return x;
+}
+
+lval* builtin(lval* a, char* func) {
+  if (strcmp("list", func) == 0) { return builtin_list(a); }
+  if (strcmp("head", func) == 0) { return builtin_head(a); }
+  if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+  if (strcmp("join", func) == 0) { return builtin_join(a); }
+  if (strcmp("eval", func) == 0) { return builtin_eval(a); }
+  if (strstr("+-/*", func)) { return builtin_op(a, func); }
+  lval_del(a);
+  return lval_err("Unknown Function!");
+}
 
 int main(int argc, char** argv) {
   
@@ -330,13 +415,14 @@ int main(int argc, char** argv) {
   mpc_parser_t* SherLang  = mpc_new("sherlang");
 
   mpca_lang(MPCA_LANG_DEFAULT,
-  "                                                    \
-    number : /[+-]?([0-9]*[.])?[0-9]+/ ;               \
-    symbol : '+' | '-' | '*' | '/' | '%';              \
-    sexpr  : '(' <expr>* ')' ;                         \
-    qexpr  : '{' <expr>* '}' ;                         \
-    expr   : <number> | <symbol> | <sexpr> | <qexpr> ; \
-    sherlang  : /^/ <expr>* /$/ ;                      \
+  "                                                             \
+    number : /[+-]?([0-9]*[.])?[0-9]+/ ;                        \
+    symbol : \"list\" | \"head\" | \"tail\"                     \
+           | \"join\" | \"eval\" | '+' | '-' | '*' | '/' | '%'; \
+    sexpr  : '(' <expr>* ')' ;                                  \
+    qexpr  : '{' <expr>* '}' ;                                  \
+    expr   : <number> | <symbol> | <sexpr> | <qexpr> ;          \
+    sherlang  : /^/ <expr>* /$/ ;                               \
   ",
   Number, Symbol, Sexpr, Qexpr, Expr, SherLang);
 
